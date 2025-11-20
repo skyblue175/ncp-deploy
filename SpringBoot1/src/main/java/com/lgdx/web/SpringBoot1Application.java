5c04d7ec126478dce05c6dce9a59e1982b9b526a
skyblue175 <alswl987543@gmail.com>
2025-11-20 12:53:00 +0900

    commit

--- SpringBoot1/src/main/java/com/lgdx/web/SpringBoot1Application.java
+++ SpringBoot1/src/main/java/com/lgdx/web/SpringBoot1Application.java
@@ -2,6 +2,8 @@ package com.lgdx.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 // @SpringBoot1Application : 해당 클래스의 위치를 기준으로 하위에 있는 파일을 읽어 
 // 							Spring Boot의 설정을 자동적으로 진행하는 클래스
@@ -9,7 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // @ComponentScan : @Controller, @RestController, @Services, @Repository
 
 @SpringBootApplication
-public class SpringBoot1Application {
+public class SpringBoot1Application extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringBoot1Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot1Application.class, args);
